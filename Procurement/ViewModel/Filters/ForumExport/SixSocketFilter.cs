7e24469c847983c542deeea391bd18900fa2de74
Stickymaddness <stickymaddness@gmail.com>
2014-05-11 01:02:08 +0200

    Added {SixSocket} filter and forum tag

---
+++ Procurement/ViewModel/Filters/ForumExport/SixSocketFilter.cs
@@ -0,0 +1,39 @@
+﻿using POEApi.Model;
+using System;
+using System.Linq;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class SixSocketFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Keyword
+        {
+            get { return "6 Socket"; }
+        }
+
+        public string Help
+        {
+            get { return "Gear with 6 sockets"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            var gear = item as Gear;
+
+            if (gear == null)
+                return false;
+
+            return gear.Sockets.Count() == 6;
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/SixSocketVisitor.cs
@@ -0,0 +1,15 @@
+﻿using POEApi.Model;
+using Procurement.ViewModel.Filters.ForumExport;
+using System.Collections.Generic;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class SixSocketVisitor : VisitorBase
+    {
+        private const string TOKEN = "{SixSocket}";
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<SixSocketFilter>(items));
+        }
+    }
+}
