6d06e6d66c4137e17a2aeb1d6c43029ed112b74a
Stickymaddness <stickymaddness@gmail.com>
2014-05-11 01:11:09 +0200

    Renamed {SixSocket} tag to {6Socket} to conform with existing tags

--- Procurement/ViewModel/ForumExportVisitors/SixSocketVisitor.cs
+++ Procurement/ViewModel/ForumExportVisitors/SixSocketVisitor.cs
@@ -6,7 +6,7 @@ namespace Procurement.ViewModel.ForumExportVisitors
 {
     internal class SixSocketVisitor : VisitorBase
     {
-        private const string TOKEN = "{SixSocket}";
+        private const string TOKEN = "{6Socket}";
         public override string Visit(IEnumerable<Item> items, string current)
         {
             return current.Replace(TOKEN, runFilter<SixSocketFilter>(items));
