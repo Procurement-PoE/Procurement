ea4252d66f88d3605a192ff2c327a2727f7be9aa
Stickymaddness <stickymaddness@gmail.com>
2014-05-13 18:44:22 +0200

    Added {AllGems}

---
+++ Procurement/ViewModel/Filters/ForumExport/AllGemsFilter.cs
@@ -0,0 +1,32 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class AllGemsFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "All gems"; }
+        }
+
+        public string Help
+        {
+            get { return "Any and all gems"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Gems; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item is Gem;
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/AllGemVisitor.cs
@@ -0,0 +1,15 @@
+﻿using POEApi.Model;
+using Procurement.ViewModel.Filters.ForumExport;
+using System.Collections.Generic;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class AllGemVisitor : VisitorBase
+    {
+        private const string TOKEN = "{AllGems}";
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<AllGemsFilter>(items));
+        }
+    }
+}
