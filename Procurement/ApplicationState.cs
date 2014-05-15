51a56a864bea975063f8414175acb38e20db5baf
Stickymaddness <stickymaddness@gmail.com>
2014-05-15 20:56:52 +0200

    Procurement 1.4.0 Release

--- Procurement/ApplicationState.cs
+++ Procurement/ApplicationState.cs
@@ -9,7 +9,7 @@ namespace Procurement
 {
     public static class ApplicationState
     {
-        public static string Version = "Procurement 1.4.0-dev";
+        public static string Version = "Procurement 1.4.0";
         public static POEModel Model = new POEModel();
         public static Dictionary<string, Stash> Stash = new Dictionary<string, Stash>();
         public static Dictionary<string, Item> Inventory = new Dictionary<string, Item>();
