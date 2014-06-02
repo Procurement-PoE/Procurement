bd977e9ff0e0cb549746d9b150867a0096dc465d
Stickymaddness <stickymaddness@gmail.com>
2014-06-02 16:22:36 +0200

    VisitorBase cleanup

--- Procurement/ViewModel/ForumExportVisitors/VisitorBase.cs
+++ Procurement/ViewModel/ForumExportVisitors/VisitorBase.cs
@@ -9,8 +9,6 @@ namespace Procurement.ViewModel.ForumExportVisitors
 {
     internal abstract class VisitorBase : IVisitor
     {
-        private const string LINKITEM = "[linkItem location=\"{0}\" league=\"{1}\" x=\"{2}\" y=\"{3}\"]";
-
         protected virtual bool buyoutItemsOnlyVisibleInBuyoutsTag
         {
             get { return Settings.UserSettings.GetEntry("BuyoutItemsOnlyVisibleInBuyoutsTag").ToLower() == "true"; }
@@ -27,7 +25,7 @@ namespace Procurement.ViewModel.ForumExportVisitors
         }
 
         public abstract string Visit(IEnumerable<Item> items, string current);
-        
+
         protected string runFilter<T>(IEnumerable<Item> items) where T : IFilter, new()
         {
             return runFilter(new T(), items);
@@ -49,22 +47,10 @@ namespace Procurement.ViewModel.ForumExportVisitors
 
             if ((onlyDisplayBuyouts && !isBuyoutItem) || (isBuyoutItem && buyoutItemsOnlyVisibleInBuyoutsTag))
                 return string.Empty;
-            
-            //if (isBuyoutItem)
-            //{
-            //    if (buyoutItemsOnlyVisibleInBuyoutsTag)
-            //        return string.Empty;
-
-            //    if (embedBuyouts)
-            //        return string.Format("\n[linkItem location=\"{0}\" league=\"{1}\" x=\"{2}\" y=\"{3}\"]\n~b/o {4}\n", item.inventoryId, ApplicationState.CurrentLeague, item.X, item.Y, getBuyout<T>(item, tabName));
-            //}
-
-            //return string.Format("[linkItem location=\"{0}\" league=\"{1}\" x=\"{2}\" y=\"{3}\"]", item.inventoryId, ApplicationState.CurrentLeague, item.X, item.Y);
-
 
             if (!(embedBuyouts && isBuyoutItem))
                 return string.Format("[linkItem location=\"{0}\" league=\"{1}\" x=\"{2}\" y=\"{3}\"]", item.inventoryId, ApplicationState.CurrentLeague, item.X, item.Y);
-            
+
             return string.Format("\n[linkItem location=\"{0}\" league=\"{1}\" x=\"{2}\" y=\"{3}\"]{4}", item.inventoryId, ApplicationState.CurrentLeague, item.X, item.Y, appendAdditionalInfo(item, tabName));
         }
 
@@ -78,7 +64,7 @@ namespace Procurement.ViewModel.ForumExportVisitors
 
             if (buyoutInfo.Buyout != string.Empty)
                 sb.Append(string.Format("\n~b/o {0}", buyoutInfo.Buyout));
-            
+
             if (buyoutInfo.CurrentOffer != string.Empty)
                 sb.Append(string.Format("\n~c/o {0}", buyoutInfo.CurrentOffer));
 
@@ -88,14 +74,6 @@ namespace Procurement.ViewModel.ForumExportVisitors
             sb.Append("\n");
 
             return sb.ToString();
-        }
-
-        private static string getBuyout<T>(T item, string tabName) where T : Item
-        {
-            if (Settings.Buyouts.ContainsKey(item.UniqueIDHash))
-                return Settings.Buyouts[item.UniqueIDHash].Buyout;
-
-            return Settings.TabsBuyouts[tabName];
         }
     }
 }
