f2660c6d6a83453a8ebe7aa1cc2cf8eeedef05a2
Stickymaddness <stickymaddness@gmail.com>
2014-06-02 20:52:33 +0200

    Updated version to 1.5.1-dev, fixed bug in ItemTradeInfo.IsEmpty

--- POEApi.Model/ItemTradeInfo.cs
+++ POEApi.Model/ItemTradeInfo.cs
@@ -5,19 +5,19 @@
         public string Buyout { get; set; }
         public string Price { get; set; }
         public string CurrentOffer { get; set; }
-        public bool IsEmpty { get; set; }
-
-        public ItemTradeInfo()
+        public bool IsEmpty
         {
-            this.IsEmpty = true;
+            get { return string.IsNullOrEmpty(Buyout) && string.IsNullOrEmpty(Price) && string.IsNullOrEmpty(CurrentOffer); }
         }
+        
+        public ItemTradeInfo()
+        { }
 
         public ItemTradeInfo(string buyout, string price, string currentOffer)
         {
             this.Buyout = buyout;
             this.Price = price;
             this.CurrentOffer = currentOffer;
-            this.IsEmpty = false;
         }
     }
 }
