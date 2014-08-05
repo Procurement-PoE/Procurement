d28f46b435999c6ac805ec5b9af2d4c93496a04d
Keeper <phidias.chiang@gmail.com>
2014-08-06 07:33:24 +0800

    Add gem counts feature

---
+++ POEApi.Model/GemHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POEApi.Model
+{
+    internal class GemHandler
+    {
+        
+
+        static GemHandler()
+        {
+         
+        }
+
+        public static SortedDictionary<string, int> GetGemDistribution(IEnumerable<Gem> gems)
+        {
+            SortedDictionary<string, int> gemTable = new SortedDictionary<string, int>(Settings.GemTable);
+            foreach (Gem gem in gems)
+            {
+                if (gemTable.ContainsKey(gem.TypeLine))
+                    gemTable[gem.TypeLine]++;
+                else
+                    gemTable[gem.TypeLine] = 1;
+            }
+            return gemTable;
+        }
+    }
+}
