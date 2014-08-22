4a1b635afa828db09bdf752a37476a043e1dae83
Stickymaddness <stickymaddness@gmail.com>
2014-08-22 11:33:55 +0200

    Added WIP support for hideout decorations

---
+++ POEApi.Model/Decoration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POEApi.Model
+{
+    public class Decoration : Item
+    {
+        public Decoration(JSONProxy.Item item)
+            : base(item)
+        {
+
+        }
+
+        protected override int getConcreteHash()
+        {
+            return 0;
+        }
+    }
+}
--- POEApi.Model/ItemFactory.cs
+++ POEApi.Model/ItemFactory.cs
@@ -12,6 +12,9 @@ namespace POEApi.Model
                 if (item.frameType == 4)
                     return new Gem(item);
 
+                if (item.DescrText != null && item.DescrText.ToLower() == "right click this item then left click a location on the ground to create the object.")
+                    return new Decoration(item);
+
                 if (item.frameType == 5)
                     return new Currency(item);
 
