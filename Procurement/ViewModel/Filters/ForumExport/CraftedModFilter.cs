6f0c319de2f9e4a1f42a8a4bd3a120c686904ca1
Stickymaddness <stickymaddness@gmail.com>
2014-09-11 20:10:44 +0200

    Added advanced search filter for items with crafted mods

---
+++ Procurement/ViewModel/Filters/ForumExport/CraftedModFilter.cs
@@ -0,0 +1,36 @@
+﻿using POEApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class CraftedModFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "Crafted Mods"; }
+        }
+
+        public string Help
+        {
+            get { return "Items with crafted mods"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item.CraftedMods != null && item.CraftedMods.Count() > 0;
+        }
+    }
+}
