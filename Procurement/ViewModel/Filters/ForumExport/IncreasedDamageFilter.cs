56efc6d5e7a1ce9774462e4aed0c84087cc1a360
Stickymaddness <stickymaddness@gmail.com>
2014-11-30 12:21:04 +0200

    Added filters for increased cold/fire/lightning

---
+++ Procurement/ViewModel/Filters/ForumExport/IncreasedDamageFilter.cs
@@ -0,0 +1,35 @@
+﻿namespace Procurement.ViewModel.Filters.ForumExport
+{
+    internal class IncreasedDamageFilter : StatFilter
+    {
+        internal IncreasedDamageFilter(string keyword, string help, params string[] stats)
+            : base(keyword, help, stats)
+        { }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Damage; }
+        }
+    }
+
+    internal class IncreasedDamageFilterCold : IncreasedDamageFilter
+    {
+        public IncreasedDamageFilterCold()
+            : base("Increased Cold Damage", "Increased Cold Damage", "Increased Cold Damage")
+        { }
+    }
+
+    internal class IncreasedDamageFilterFire : IncreasedDamageFilter
+    {
+        public IncreasedDamageFilterFire()
+            : base("Increased Fire Damage", "Increased Fire Damage", "Increased Fire Damage")
+        { }
+    }
+
+    internal class IncreasedDamageFilterLightning : IncreasedDamageFilter
+    {
+        public IncreasedDamageFilterLightning()
+            : base("Increased Lightning Damage", "Increased Lightning Damage", "Increased Lightning Damage")
+        { }
+    }
+}
