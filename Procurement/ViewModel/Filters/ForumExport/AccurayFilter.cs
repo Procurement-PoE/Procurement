c64f202622ce48c2054d17609cd80120aa2948f0
Stickymaddness <stickymaddness@gmail.com>
2014-11-30 12:11:36 +0200

    Added accuracy filter

---
+++ Procurement/ViewModel/Filters/ForumExport/AccurayFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    internal class AccurayFilter : StatFilter
+    {
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Attacks; }
+        }
+
+        public AccurayFilter()
+            : base("Increased Accuracy", "Increased Accuracy", "Accuracy")
+        { }
+    }
+}
