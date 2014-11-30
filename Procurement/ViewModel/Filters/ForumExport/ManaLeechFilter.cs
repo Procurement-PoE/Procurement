161979c0f0fec4f812cb9c80d78e5c37ba63647e
Stickymaddness <stickymaddness@gmail.com>
2014-11-30 12:13:50 +0200

    Added mana leech filter

---
+++ Procurement/ViewModel/Filters/ForumExport/ManaLeechFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace Procurement.ViewModel.Filters.ForumExport
+{
+    internal class ManaLeech : StatFilter
+    {
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public ManaLeech()
+            : base("Mana leech", "Items with mana leech", "Leeched as mana")
+        { }
+    }
+}
