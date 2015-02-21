0e508654efc5035f1d3cf767c6115f4d10eb38d7
David Dowdle <daviddowdle@gmail.com>
2015-02-21 11:19:27 -0500

    Auto offline enhancement
    Added logic to use the new poe.xyz offline feature that expands on the
    previous change that Kapps made. The user will only be set as online if
    POE is running and they haven't been AFK for more than 10 minutes. The
    enhancement is that once POE is closed or the user goes AFK then they
    are set to be offline. The main online timer was set to run at 1 minute
    to help with this, and extra logic was added so that we don't send
    multiple repeated online or offline commands to poe.xyz.
    
    File needs to be cleaned up before merge since I reformatted it to my
    normal style.

--- Procurement/Utility/PoeTradeOnlineHelper.cs
+++ Procurement/Utility/PoeTradeOnlineHelper.cs
@@ -11,102 +11,123 @@ using System.Linq;
 
 namespace Procurement.Utility
 {
-    class PoeTradeOnlineHelper
-    {
+   class PoeTradeOnlineHelper
+   {
 
-        private static PoeTradeOnlineHelper instance;
-        private System.Timers.Timer refreshTimer;
-        private Uri refreshUri;
+      private static PoeTradeOnlineHelper instance;
+      private System.Timers.Timer refreshTimer;
+      private Uri refreshUri;
 
-        [DllImport("user32.dll")]
-        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+      [DllImport("user32.dll")]
+      private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
 
-        [StructLayout(LayoutKind.Sequential)]
-        private struct LASTINPUTINFO
-        {
-            public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
-            [MarshalAs(UnmanagedType.U4)]
-            public UInt32 cbSize;
-            [MarshalAs(UnmanagedType.U4)]
-            public UInt32 dwTime;
-        }
+      [StructLayout(LayoutKind.Sequential)]
+      private struct LASTINPUTINFO
+      {
+         public static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+         [MarshalAs(UnmanagedType.U4)]
+         public UInt32 cbSize;
+         [MarshalAs(UnmanagedType.U4)]
+         public UInt32 dwTime;
+      }
 
-        private PoeTradeOnlineHelper()
-        {
-            refreshTimer = new Timer();
-            refreshTimer.Elapsed += (s, e) => { RefreshOnlineStatus(); };
-            refreshTimer.Interval = 30 * 60 * 1000;
-        }
+      private PoeTradeOnlineHelper()
+      {
+         refreshTimer = new Timer();
+         refreshTimer.Elapsed += (s, e) => { RefreshOnlineStatus(); };
+         refreshTimer.Interval = 1 * 60 * 1000;
+      }
 
-        public static PoeTradeOnlineHelper Instance
-        {
-            get
+      public static PoeTradeOnlineHelper Instance
+      {
+         get
+         {
+            if (instance == null)
+               instance = new PoeTradeOnlineHelper();
+
+            return instance;
+         }
+      }
+
+      private bool currentlyOnline;
+      private void RefreshOnlineStatus()
+      {
+         try
+         {
+            Func<Process, bool> IsPoE = (c => c.MainWindowTitle.Contains("Path of Exile") || c.ProcessName.Contains("PathOfExile"));
+            var idleTime = GetIdleTime();
+
+            if (idleTime >= TimeSpan.FromMinutes(10) || !Process.GetProcesses().Any(IsPoE))
             {
-                if (instance == null)
-                    instance = new PoeTradeOnlineHelper();
+               // Prevent from spamming poe.xyz
+               if (currentlyOnline)
+               {
+                  using (var client = new WebClient())
+                  {
+                     var offlineUri = new Uri(refreshUri.OriginalString + "/offline");
+                     var data = new NameValueCollection();
+                     client.UploadValuesAsync(offlineUri, "POST", data);
+                  }
+               }
 
-                return instance;
+               // User is AFK or PoE is not running.
+               currentlyOnline = false;
+               return;
             }
-        }
 
-        private void RefreshOnlineStatus()
-        {
-            try
+            if (!currentlyOnline)
             {
-                Func<Process, bool> IsPoE = (c => c.MainWindowTitle.Contains("Path of Exile") || c.ProcessName.Contains("PathOfExile"));
-                if(GetIdleTime() >= TimeSpan.FromMinutes(10) || !Process.GetProcesses().Any(IsPoE))
-                {
-                    // User is AFK or PoE is not running.
-                    return;
-                }
-                using (var client = new WebClient())
-                {
-                    var data = new NameValueCollection();
-                    client.UploadValuesAsync(refreshUri, "POST", data);
-                }
+               using (var client = new WebClient())
+               {
+                  var data = new NameValueCollection();
+                  client.UploadValuesAsync(refreshUri, "POST", data);
+                  currentlyOnline = true;
+               }
             }
-            catch (Exception ex)
-            {
-                Logger.Log("Error refreshing online status in PoeTradeOnlineHelper: " + ex.ToString());
-            }
-        }
+         }
+         catch (Exception ex)
+         {
+            Logger.Log("Error refreshing online/offline status in PoeTradeOnlineHelper: " + ex.ToString());
+         }
+      }
 
-        private TimeSpan GetIdleTime()
-        {
-            var inputInfo = new LASTINPUTINFO() {
-                cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO)),
-                dwTime = 0
-            };
-            GetLastInputInfo(ref inputInfo);
-            // Allow for TickCount wrap-around.
-            return TimeSpan.FromMilliseconds(unchecked(Environment.TickCount - inputInfo.dwTime));
-        }
+      private TimeSpan GetIdleTime()
+      {
+         var inputInfo = new LASTINPUTINFO()
+         {
+            cbSize = (uint)Marshal.SizeOf(typeof(LASTINPUTINFO)),
+            dwTime = 0
+         };
+         GetLastInputInfo(ref inputInfo);
+         // Allow for TickCount wrap-around.
+         return TimeSpan.FromMilliseconds(unchecked(Environment.TickCount - inputInfo.dwTime));
+      }
 
-        internal void Start()
-        {
-            try
-            {
-                var enabled = Convert.ToBoolean(Settings.UserSettings["PoeTradeRefreshEnabled"]);
+      internal void Start()
+      {
+         try
+         {
+            var enabled = Convert.ToBoolean(Settings.UserSettings["PoeTradeRefreshEnabled"]);
 
-                if (!enabled || refreshTimer.Enabled)
-                    return;
+            if (!enabled || refreshTimer.Enabled)
+               return;
 
-                var poeTradeUrl = Settings.UserSettings["PoeTradeRefreshUrl"];
-                refreshUri = new Uri(poeTradeUrl);
+            var poeTradeUrl = Settings.UserSettings["PoeTradeRefreshUrl"];
+            refreshUri = new Uri(poeTradeUrl);
 
-                refreshTimer.Start();
-                RefreshOnlineStatus();
-            }
-            catch (Exception ex)
-            {
-                Logger.Log("Error starting PoeTradeOnlineHelper: " + ex.ToString());
-                MessageBox.Show("Error refreshing poe.trade online status, ensure you have a valid url entered.", "Error Starting PoeTradeOnlineHelper", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
+            refreshTimer.Start();
+            RefreshOnlineStatus();
+         }
+         catch (Exception ex)
+         {
+            Logger.Log("Error starting PoeTradeOnlineHelper: " + ex.ToString());
+            MessageBox.Show("Error refreshing poe.trade online status, ensure you have a valid url entered.", "Error Starting PoeTradeOnlineHelper", MessageBoxButton.OK, MessageBoxImage.Error);
+         }
+      }
 
-        internal void Stop()
-        {
-            refreshTimer.Stop();
-        }
-    }
+      internal void Stop()
+      {
+         refreshTimer.Stop();
+      }
+   }
 }
