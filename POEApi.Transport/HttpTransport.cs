61988c72911f8429f15881311dd7b1f0210206bf
Philippe <philippe.lord.temp@gmail.com>
2015-04-27 18:29:19 -0400

    Update HttpTransport.cs
    Enabled gzipped data transfers.

--- POEApi.Transport/HttpTransport.cs
+++ POEApi.Transport/HttpTransport.cs
@@ -109,7 +109,9 @@ namespace POEApi.Transport
             request.UserAgent = "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; .NET CLR 1.1.4322)";
             request.Method = method.ToString();
             request.Proxy = getProxySettings();
-
+            // data will be sent in clear-text unless this is explicitely specified
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            
             request.ContentType = "application/x-www-form-urlencoded";
 
             return request;
