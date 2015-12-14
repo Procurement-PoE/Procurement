38efd1f6d16637a972dbb5a4f28016748a92f1e2
Stickymaddness <stickymaddness@gmail.com>
2015-12-14 12:00:01 +0200

    Updated AllGemsFilter and LeveledGemsFilter to include socketed gems in results

--- Procurement/ViewModel/Filters/ForumExport/AllGemsFilter.cs
+++ Procurement/ViewModel/Filters/ForumExport/AllGemsFilter.cs
@@ -1,4 +1,5 @@
 ﻿using POEApi.Model;
+using System.Linq;
 
 namespace Procurement.ViewModel.Filters.ForumExport
 {
@@ -26,6 +27,10 @@ namespace Procurement.ViewModel.Filters.ForumExport
 
         public bool Applicable(Item item)
         {
+            Gear gear = item as Gear;
+            if (gear != null && gear.SocketedItems.Any(x => Applicable(x)))
+                return true;
+
             return item is Gem;
         }
     }
--- Procurement/ViewModel/Filters/ForumExport/LeveledGemFilter.cs
+++ Procurement/ViewModel/Filters/ForumExport/LeveledGemFilter.cs
@@ -1,4 +1,5 @@
 ﻿using POEApi.Model;
+using System.Linq;
 
 namespace Procurement.ViewModel.Filters
 {
@@ -37,6 +38,10 @@ namespace Procurement.ViewModel.Filters
 
         public bool Applicable(POEApi.Model.Item item)
         {
+            Gear gear = item as Gear;
+            if (gear != null && gear.SocketedItems.Any(x => Applicable(x)))
+                return true;
+            
             Gem gem = item as Gem;
             if (gem == null)
                 return false;
