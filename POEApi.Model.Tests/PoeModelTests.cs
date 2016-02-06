cd292aa11ef8cd90663f6856a6fcb27b57d7d080
aydjay <aydjay@gmail.com>
2016-02-06 12:58:52 +0000

    Added unit test for GetStash before refactoring to use JSon.Net

--- POEApi.Model.Tests/PoeModelTests.cs
+++ POEApi.Model.Tests/PoeModelTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using POEApi.Transport;
@@ -8,29 +9,44 @@ namespace POEApi.Model.Tests
     [TestClass]
     public class PoeModelTests
     {
-        //[TestMethod]
-        //public void GetStashTest()
-        //{
-        //    POEModel model = new POEModel();
+        private Mock<ITransport> _mockTransport;
+        private POEModel _model;
 
-        //    Mock<ITrans
-        //    model.GetStash("Talisman", "d347hm4n");
-        //}
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _mockTransport = new Mock<ITransport>();
+            _model = new POEModel { Transport = _mockTransport.Object };
+        }
+
+        [TestMethod]
+        public void GetStashTest()
+        {
+            //Nasty little gotcha See: http://blogs.msdn.com/b/cie/archive/2014/03/19/encountered-unexpected-character-239-error-serializing-json.aspx
+            //You have to remove the first three bytes from the file.
+            string fakeStashInfo = Encoding.UTF8.GetString(Files.SampleStash);
+            using (var stream = GenerateStreamFromString(fakeStashInfo))
+            {
+                _mockTransport.Setup(m => m.GetStash(0, "", "", false)).Returns(stream);
+
+                var stash = _model.GetStash(0, "", "");
+
+                Assert.IsNotNull(stash);
+
+                Assert.AreEqual(stash.Tabs.Count, 17);
+            }
+        }
 
         [TestMethod]
         public void GetAccountNameTest()
         {
-            var mockTransport = new Mock<ITransport>();
-            
             var fakeAccountNameResponse = "{\"accountName\":\"fakeAccountName\"}";
 
             using (var stream = GenerateStreamFromString(fakeAccountNameResponse))
             {
-                mockTransport.Setup(m => m.GetAccountName()).Returns(stream);
+                _mockTransport.Setup(m => m.GetAccountName()).Returns(stream);
 
-                var model = new POEModel {Transport = mockTransport.Object};
-
-                var account = model.GetAccountName();
+                var account = _model.GetAccountName();
 
                 Assert.AreEqual(account, "fakeAccountName");
             }
