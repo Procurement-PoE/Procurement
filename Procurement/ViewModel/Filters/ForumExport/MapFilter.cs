a5eeef503a102a544d37cacc5b4b5d87d2b89da8
M <tobananer@gmail.com>
2016-04-27 11:17:34 +0200

    Added option to filter maps

---
+++ Procurement/ViewModel/Filters/ForumExport/MapFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class MapFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "Map"; }
+        }
+
+        public string Help
+        {
+            get { return "Any and all maps of any tier"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item is Map;
+        }
+    }
+}
