7abf026d9430be08a127209d9f01ada100861482
Stickymaddness <stickymaddness@gmail.com>
2016-05-21 13:38:49 +0200

    Added % Life/ES filters

---
+++ Procurement/ViewModel/Filters/ForumExport/PercentEnergyShieldFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class PercentEnergyShieldFilter : StatFilter
+    {
+        public PercentEnergyShieldFilter()
+            : base("% Increased Energy Shield", "Items with % increased energy shield", "% increased maximum energy shield")
+        { }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+    }
+}
---
+++ Procurement/ViewModel/Filters/ForumExport/PercentLifeFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class PercentLifeFilter : StatFilter
+    {
+        public PercentLifeFilter()
+            : base("% Increased Life", "Items with % increased life", "% increased maximum life")
+        { }
+
+        public override FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+    }
+}
