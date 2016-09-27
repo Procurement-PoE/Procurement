c3c23ea1ace322866e63843f5841dca474219c85
Stickymaddness <stickymaddness@gmail.com>
2016-09-27 12:17:01 +0200

    Updates version to dev

--- Procurement/ApplicationState.cs
+++ Procurement/ApplicationState.cs
@@ -10,7 +10,7 @@ namespace Procurement
 {
     public static class ApplicationState
     {
-        public static string Version = "Procurement 1.9.9";
+        public static string Version = "Procurement 1.9.10-dev";
         public static POEModel Model = new POEModel();
         public static Dictionary<string, Stash> Stash = new Dictionary<string, Stash>();
         public static Dictionary<string, Item> Inventory = new Dictionary<string, Item>();
