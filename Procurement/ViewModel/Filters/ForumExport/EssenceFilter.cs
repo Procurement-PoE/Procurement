fc03406ce43fa9bcbbb389854c5a462cf5dbeff2
Stickymaddness <stickymaddness@gmail.com>
2016-09-27 15:13:37 +0200

    Added support for essences

---
+++ Procurement/ViewModel/Filters/ForumExport/EssenceFilter.cs
@@ -0,0 +1,44 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class EssenceFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Keyword
+        {
+            get
+            {
+                return "Essence";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "All Essences";
+            }
+        }
+
+        public FilterGroup Group
+        {
+            get
+            {
+                return FilterGroup.Default;
+            }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item.TypeLine.ToLower().Contains("essence");
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/EssenceVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using POEApi.Model;
+
+using Procurement.ViewModel.Filters.ForumExport;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class EssenceVisitor : VisitorBase
+    {
+        private const string TOKEN = "{Essence}";
+
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<EssenceFilter>(items.OrderBy(i => i.TypeLine)));
+        }
+    }
+}
