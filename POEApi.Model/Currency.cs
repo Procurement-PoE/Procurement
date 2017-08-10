e6c1ebdf79d7d41a065d9fddf8d4d25c8929ef95
aydjay <aydjay@gmail.com>
2017-08-10 11:02:16 +0100

    Added support for Harbinger Currencies

--- POEApi.Model/Currency.cs
+++ POEApi.Model/Currency.cs
@@ -1,5 +1,8 @@
-﻿namespace POEApi.Model
+﻿using System.Diagnostics;
+
+namespace POEApi.Model
 {
+    [DebuggerDisplay("Type: {Type} Stack: {StackInfo.Amount}/{StackInfo.MaxSize}")]
     public class Currency : StackableItem
     {
         public Currency(JSONProxy.Item item) : base(item)
