c329006eb2666ff1c5e89d9b58418514e4d3040e
Charles Macanka <cmacanka@gmail.com>
2018-02-18 15:20:55 -0500

    Include number of complete recipes in description.
    A "complete" recipe is one where the PercentMatch is exactly 100; any
    PercentMatch value that is lower or higher can be improved.

--- Procurement/View/Converters/RecipeDescriptionConverter.cs
+++ Procurement/View/Converters/RecipeDescriptionConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Data;
 using Procurement.ViewModel.Recipes;
 
@@ -10,7 +11,8 @@ namespace Procurement.View
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             KeyValuePair<string, List<RecipeResult>> item = (KeyValuePair<string, List<RecipeResult>>)value;
-            return string.Format("{0} ({1} Results)", item.Key, item.Value.Count);
+            int numCompleteResults = item.Value.Count(i => i.PercentMatch == 100);
+            return string.Format("{0} ({1}/{2} Results)", item.Key, numCompleteResults, item.Value.Count);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
