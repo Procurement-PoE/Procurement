cccee7bd9913952ffe1026182b1280b29177d18a
Charles Macanka <cmacanka@gmail.com>
2018-02-18 13:04:13 -0500

    Replace RequestThrottle with a simpler version.
    The RequestThrottle class is unnecessarily tied to creating WebRequest
    objects when starting a task, where the class only needs to make sure
    the requests are gated properly.  The RequestThrottle class also
    enforces a singleton pattern, where TaskThrottle does not.  This means
    whatever uses TaskThrottle has to manage a single instance on its own,
    as necessary.  This does mean, however, you can use multiple instances
    of TaskThrottle to throttle unrelated sets of taks independently.

---
+++ POEApi.Transport/TaskThrottle.cs
@@ -0,0 +1,94 @@
+﻿using POEApi.Infrastructure.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POEApi.Transport
+{
+    public class TaskThrottle
+    {
+        protected Queue<DateTime> CurrentTasks { get; set; }
+        public TimeSpan WindowSize { get; set; }
+        public int WindowLimit { get; protected set; }
+        public int SimultaneiousTasksLimit { get; protected set; }
+
+        private int _numberOfOutstandingTasks;
+        public int NumberOfOutstandingTasks
+        {
+            get
+            {
+                return _numberOfOutstandingTasks;
+            }
+            protected set
+            {
+                _numberOfOutstandingTasks = value;
+            }
+        }
+
+        public event ThottledEventHandler Throttled;
+
+        private Object lockObject = new Object();
+
+        public TaskThrottle(TimeSpan windowSize, int windowLimit, int simultaneiousTasksLimit)
+        {
+            CurrentTasks = new Queue<DateTime>(simultaneiousTasksLimit);
+            WindowSize = windowSize;
+            WindowLimit = windowLimit;
+            SimultaneiousTasksLimit = simultaneiousTasksLimit;
+        }
+
+        public void StartTask()
+        {
+            bool finished = false;
+            while (!finished)
+            {
+                while (NumberOfOutstandingTasks >= SimultaneiousTasksLimit)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                lock(lockObject)
+                {
+                    if (NumberOfOutstandingTasks >= SimultaneiousTasksLimit)
+                    {
+                        // Another thread added a task to the queue before we could get the lock.
+                        continue;
+                    }
+
+                    RemvoeExpiredTasks();
+                    if (CurrentTasks.Count == WindowLimit)
+                    {
+                        TimeSpan waitTime = CurrentTasks.Dequeue() - DateTime.Now;
+                        if (waitTime.TotalMilliseconds > 0)
+                        {
+                            Throttled?.Invoke(this, new ThottledEventArgs(waitTime));
+                            System.Threading.Thread.Sleep(waitTime);
+                        }
+                    }
+
+                    System.Threading.Interlocked.Increment(ref _numberOfOutstandingTasks);
+                    CurrentTasks.Enqueue(DateTime.Now + WindowSize);
+                    finished = true;
+                }
+            }
+        }
+
+        public void CompleteTask()
+        {
+            lock(lockObject)
+            {
+                System.Threading.Interlocked.Decrement(ref _numberOfOutstandingTasks);
+                RemvoeExpiredTasks();
+            }
+        }
+
+        protected void RemvoeExpiredTasks()
+        {
+            while (CurrentTasks.Count > 0 && CurrentTasks.Peek() <= DateTime.Now)
+            {
+                CurrentTasks.Dequeue();
+            }
+        }
+    }
+}
