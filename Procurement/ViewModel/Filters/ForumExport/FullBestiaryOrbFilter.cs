162d349b6f91f4632d82cc80a856db2f41362ba2
Charles Macanka <cmacanka@gmail.com>
2018-05-06 20:36:38 -0400

    Added filters and visitors for full bestiary orbs.

---
+++ Procurement/ViewModel/Filters/ForumExport/FullBestiaryOrbFilter.cs
@@ -0,0 +1,44 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class FullBestiaryOrbFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Keyword
+        {
+            get
+            {
+                return "Full Bestiary Orb";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "All Bestiary Orbs Containing Beasts";
+            }
+        }
+
+        public FilterGroup Group
+        {
+            get
+            {
+                return FilterGroup.Default;
+            }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item is FullBestiaryOrb;
+        }
+    }
+}
