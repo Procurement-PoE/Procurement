77f750b46db2ee1c79686fb1fd3b176fc2dc8aab
Charles Macanka <cmacanka@gmail.com>
2018-05-20 18:13:05 -0400

    Use a default net tier of 0 if a Net has none.
    Necromancy nets technically do not have a net tier.

--- POEApi.Model/Net.cs
+++ POEApi.Model/Net.cs
@@ -12,6 +12,7 @@ namespace POEApi.Model
             this.NetTier = ProxyMapper.GetNetTier(item.Properties);
         }
 
+        // Note: Necromancy nets technically do not have a net tier, but uses the default value of 0 here.
         public int NetTier { get; }
     }
 }
