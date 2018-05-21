7e5955b47a757bb1292125c06ce6e00797b527d6
Charles Macanka <cmacanka@gmail.com>
2018-05-20 21:07:48 -0400

    Added visitor and filter for UnknownItem.
    This is mostly as a debugging tool.  If any items show up in this
    filter, then there's an outstanding bug with Procurement that needs to
    be fixed.

---
+++ Procurement/ViewModel/Filters/ForumExport/UnknownItemFilter.cs
@@ -0,0 +1,44 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class UnknownItemFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Keyword
+        {
+            get
+            {
+                return "Unknown Item";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "All Items Procurement Cannot Identify";
+            }
+        }
+
+        public FilterGroup Group
+        {
+            get
+            {
+                return FilterGroup.Default;
+            }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item is UnknownItem;
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/UnknownItemVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using POEApi.Model;
+using Procurement.ViewModel.Filters.ForumExport;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class UnknownItemVisitor : VisitorBase
+    {
+        private const string TOKEN = "{UnknownItems}";
+
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            if (current.IndexOf(TOKEN) < 0)
+                return current;
+
+            return current.Replace(TOKEN, runFilter<UnknownItemFilter>(items.OrderBy(i => i.H)));
+        }
+    }
+}
