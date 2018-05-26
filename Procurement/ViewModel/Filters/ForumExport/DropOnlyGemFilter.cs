7cc09d989d2ac65e1e5cf9ff1c305506fb783c3a
Charles Macanka <cmacanka@gmail.com>
2018-05-26 16:02:12 -0400

    Updated list of drop-only gems, and look in Gear.
    The list of drop-only gems was out of date, and also did not include
    "Support" suffix on support gems, so those would not be found, anyway.
    Also updated the filter to look at the gems socketed in Gear for
    matches.

--- Procurement/ViewModel/Filters/ForumExport/DropOnlyGemFilter.cs
+++ Procurement/ViewModel/Filters/ForumExport/DropOnlyGemFilter.cs
@@ -19,11 +19,12 @@ namespace Procurement.ViewModel.Filters
         {
             //From http://en.pathofexilewiki.com/wiki/Drop_Only_Gems
             dropOnly = new List<string>();
-            dropOnly.Add("Empower");
+            dropOnly.Add("Added Chaos Damage Support");
+            dropOnly.Add("Detonate Mines");
+            dropOnly.Add("Empower Support");
+            dropOnly.Add("Enhance Support");
+            dropOnly.Add("Enlighten Support");
             dropOnly.Add("Portal");
-            dropOnly.Add("Reduced Duration");
-            dropOnly.Add("Slower Projectiles");
-            dropOnly.Add("Enhance");
         }
 
         public bool CanFormCategory
@@ -33,16 +34,20 @@ namespace Procurement.ViewModel.Filters
 
         public string Keyword
         {
-            get { return "Drop Only Gems"; }
+            get { return "Drop-Only Gems"; }
         }
 
         public string Help
         {
-            get { return "Gems only which can only be aquired through drops"; }
+            get { return "Gems which can only be aquired through drops"; }
         }
 
         public bool Applicable(Item item)
         {
+            Gear gear = item as Gear;
+            if (gear != null && gear.SocketedItems.Any(x => Applicable(x)))
+                return true;
+
             Gem gem = item as Gem;
             if (gem == null)
                 return false;
