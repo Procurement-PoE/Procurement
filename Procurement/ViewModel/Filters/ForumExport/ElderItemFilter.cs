7ed6d68a8c2269626ea3dda8fc0366f142cd3f3e
Charles Macanka <cmacanka@gmail.com>
2018-05-26 16:20:54 -0400

    Added filters and visitors for shaper/elder items.

---
+++ Procurement/ViewModel/Filters/ForumExport/ElderItemFilter.cs
@@ -0,0 +1,44 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class ElderItemFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Keyword
+        {
+            get
+            {
+                return "Elder Items";
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "All Elder Items";
+            }
+        }
+
+        public FilterGroup Group
+        {
+            get
+            {
+                return FilterGroup.Default;
+            }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item.Elder;
+        }
+    }
+}
