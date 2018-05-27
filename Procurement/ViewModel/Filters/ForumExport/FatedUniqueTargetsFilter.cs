e7bc761833cd60d78efe6e529a56964ede7ce4fb
Charles Macanka <cmacanka@gmail.com>
2018-05-27 16:09:11 -0400

    Added filters and visitors for fated uniques.
    These find uniques that can be upgraded, uniques that have already been
    upgraded, or unidentified uniques which share a base type with a fated
    unique.

---
+++ Procurement/ViewModel/Filters/ForumExport/FatedUniqueTargetsFilter.cs
@@ -0,0 +1,38 @@
+﻿using POEApi.Model;
+using System.Linq;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class FatedUniqueTargetsFilter : IFilter
+    {
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "Fated Unique Targets"; }
+        }
+
+        public string Help
+        {
+            get { return "Unique items that can be upgraded via Prophecy."; }
+        }
+
+        public bool Applicable(POEApi.Model.Item item)
+        {
+            Gear gear = item as Gear;
+            if (gear == null || gear.Rarity != Rarity.Unique || !gear.Identified)
+                return false;
+
+            return Settings.FatedUniques.Any(u => string.Equals(u.TargetItemName, gear.Name,
+                System.StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
