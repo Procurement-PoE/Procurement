13eb5a10cb39e963c66cc1ef66f424d5406e4e6f
aydjay <aydjay@gmail.com>
2018-12-17 12:29:35 +0000

    Convert Colour to WPF and seperate class to it's own file

---
+++ POEApi.Model/Colour.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace POEApi.Model
+{
+    public class Colour
+    {
+        public int r { get; set; }
+        public int g { get; set; }
+        public int b { get; set; }
+
+        public Color WpfColor => Color.FromRgb((byte) r,(byte) g,(byte) b); 
+    }
+}
