395b01bd9df8907068c590bf5df02e99fd09b928
Charles Macanka <cmacanka@gmail.com>
2018-12-22 17:57:51 -0500

    Add filters for items with veiled mods.

---
+++ Procurement/ViewModel/Filters/ForumExport/MultipleVeiledModsFilter.cs
@@ -0,0 +1,32 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class MultipleVeiledModsFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "Multiple Veiled Mods"; }
+        }
+
+        public string Help
+        {
+            get { return "Items with more than one veiled mod"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item.VeiledMods?.Count > 1;
+        }
+    }
+}
---
+++ Procurement/ViewModel/Filters/ForumExport/VeiledModFilter.cs
@@ -0,0 +1,32 @@
+﻿using POEApi.Model;
+
+namespace Procurement.ViewModel.Filters.ForumExport
+{
+    public class VeiledModFilter : IFilter
+    {
+        public bool CanFormCategory
+        {
+            get { return false; }
+        }
+
+        public string Keyword
+        {
+            get { return "Veiled Mods"; }
+        }
+
+        public string Help
+        {
+            get { return "Items with veiled mods"; }
+        }
+
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool Applicable(Item item)
+        {
+            return item.VeiledMods?.Count > 0;
+        }
+    }
+}
