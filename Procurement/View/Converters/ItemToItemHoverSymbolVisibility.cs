0dd41459b78d59be04d78a05fbcc9e3fdf206894
Charles Macanka <cmacanka@gmail.com>
2018-12-22 19:01:45 -0500

    Add converters for item hovers' symbols.
    These converters determine if any item hover symbols (Elder, Shaper, or
    Veiled) should be shown, and provide the path to the appropriate symbol
    image file.

---
+++ Procurement/View/Converters/ItemToItemHoverSymbolVisibility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using POEApi.Model;
+
+namespace Procurement.View
+{
+    public class ItemToItemHoverSymbolVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as Item;
+            if (item == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            if (item.Shaper || item.Elder || item.VeiledMods?.Count > 0)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
