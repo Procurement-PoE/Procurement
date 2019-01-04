52fc3b4559b50e313516127e7177f2d1764b1424
aydjay <aydjay@gmail.com>
2019-01-04 09:54:19 +0000

    Support Vaal Orb Recipe

--- Procurement/ViewModel/Recipes/RecipeManager.cs
+++ Procurement/ViewModel/Recipes/RecipeManager.cs
@@ -28,7 +28,7 @@ namespace Procurement.ViewModel.Recipes
                 new SameBaseTypeRecipe(),
                 new SameNameRecipe("Alchemy Orb - 3 Of The Same Name", 3),
                 new SameNameRecipe("Chance Orb - 2 Of The Same Name", 2),
-                //Todo: Implement Essence Combination recipe (Exclude Shrieking and "Special" essences
+                new VaalOrbRecipe()
             };
         }
 
