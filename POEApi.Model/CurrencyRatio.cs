c336cbaff6d8ee50d2de79b96a715ae4aa22fd30
Stickymaddness <stickymaddness@gmail.com>
2019-03-02 00:58:08 +0100

    Fix currency breakdown not displaying

--- POEApi.Model/CurrencyRatio.cs
+++ POEApi.Model/CurrencyRatio.cs
@@ -6,11 +6,11 @@
         public double OrbAmount { get; set; }
         public double ChaosAmount { get; set; }
 
-        public CurrencyRatio(OrbType orbType, double OrbAmount, double ChaosAmount)
+        public CurrencyRatio(OrbType orbType, double orbAmount, double chaosAmount)
         {
             OrbType = orbType;
-            OrbAmount = OrbAmount;
-            ChaosAmount = ChaosAmount;
+            OrbAmount = orbAmount;
+            ChaosAmount = chaosAmount;
         }
     }
 }
