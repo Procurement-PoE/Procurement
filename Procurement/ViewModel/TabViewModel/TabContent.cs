38a4c4681327f878e70aa5300465bdcbaf0dae73
Stickymaddness <stickymaddness@gmail.com>
2019-03-02 17:18:17 +0100

    Update tab dropdown to use tab colours

---
+++ Procurement/ViewModel/TabViewModel/TabContent.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using Procurement.Interfaces;
+
+namespace Procurement.ViewModel
+{
+    internal class TabContent
+    {
+        public int Index { get; set; }
+        public TabItem TabItem { get; set; }
+        public IStashControl Stash { get; set; }
+
+        public TabContent(int index, TabItem tabItem, IStashControl stash)
+        {
+            Index = index;
+            TabItem = tabItem;
+            Stash = stash;
+        }
+    }
+}
