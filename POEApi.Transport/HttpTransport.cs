fefd7518e2e63e5a650407c27a4488ece8db9fa9
Omga4000 <kfir17081993@gmail.com>
2022-12-22 00:19:03 -0800

    [#1140,#1141,#1142,#1144,#1146,#1147,#1148] Temporary fix for "Error downloading account name".

--- POEApi.Transport/HttpTransport.cs
+++ POEApi.Transport/HttpTransport.cs
@@ -27,7 +27,7 @@ namespace POEApi.Transport
 
         private const string LoginURL = @"https://www.pathofexile.com/login";
         private const string AccountURL = @"https://www.pathofexile.com/my-account";
-        private const string AccountNameURL = @"https://api.poe.watch/char?name={0}&realm={1}";
+        private const string AccountNameURL = @"https://api.poe.watch/account?name={0}&realm={1}";
         private const string CharacterURL = @"https://www.pathofexile.com/character-window/get-characters?&realm={0}";
         private const string StashURL = @"https://www.pathofexile.com/character-window/get-stash-items?league={0}&tabs=1&tabIndex={1}&accountName={2}&realm={3}";
         private const string InventoryURL = @"https://www.pathofexile.com/character-window/get-items?character={0}&accountName={1}&realm={2}";
